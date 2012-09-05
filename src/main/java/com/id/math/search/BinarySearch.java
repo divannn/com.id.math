1c2639b7fc2b6544cd375dcedc9c39c5fa231d55
divannn <unique-id@mail.ru>
2012-09-05 19:50:08 +0400

    Added search pack

--- src/main/java/com/id/math/search/BinarySearch.java
+++ src/main/java/com/id/math/search/BinarySearch.java
@@ -1,11 +1,39 @@
 package com.id.math.search;
 
-/**
- * Created with IntelliJ IDEA.
- * User: danili
- * Date: 05.09.12
- * Time: 19:32
- * To change this template use File | Settings | File Templates.
- */
 public class BinarySearch {
+
+    public static void main(String[] args) {
+        int[] data = {1, 30, -5, 6, 8, 4, 64, 6, 8, 0, 2, 4, 3};
+        int ind = binarySearch(data, 61);
+        System.err.println(">>> ind:  " + ind);
+    }
+
+    public static int binarySearch(int[] array, int target) {
+        if (array == null) {
+            return -1;
+        }
+        int left = 0;
+        int right = array.length;
+        int middle = (left + right) / 2;
+
+        while (true) {
+            if (array[middle] == target)   {
+                return middle;
+            }
+
+            if (left == middle || right == middle) {
+                return -1;
+            }
+
+            if (array[middle] < target) {
+                left = middle;
+                middle = (left + right) / 2;
+            }
+
+            if (array[middle] > target) {
+                right = middle;
+                middle = (left + right) / 2;
+            }
+        }
+    }
 }
