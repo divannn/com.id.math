0e3bd0aaca290d5879e71a80a5b7403e5749b47e
divannn <unique-id@mail.ru>
2012-09-07 18:11:27 +0400

    Added ArrayMediana.

---
+++ src/main/java/com/id/math/util/ArrayMediana.java
@@ -0,0 +1,20 @@
+package com.id.math.util;
+
+/**
+ *  Дан массив int a[n], найти его медиану (т.е. такое число, что половина элементов массива имеют
+ *  большее значение, а половина — меньшее). Желательно затратить на процедуру O(n) времени.
+ */
+public class ArrayMediana {
+
+    public static void main(String[] args) {
+        int [] date = {};
+        System.err.println("mediana: " + findMediana());
+    }
+
+    private static int findMediana(int [] data) {
+        if (data == null) {
+            return -1;
+        }
+        return  1;
+    }
+}
