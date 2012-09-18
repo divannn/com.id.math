1a843181f1bbaccf31db451bd4b1bbfe96987cc4
divannn <unique-id@mail.ru>
2012-09-18 12:50:23 +0400

    Added 2nd way of calc GCD.

--- src/main/java/com/id/math/util/GCD.java
+++ src/main/java/com/id/math/util/GCD.java
@@ -1,13 +1,26 @@
 package com.id.math.util;
 
 public class GCD {
-    /**
-     * Finds Greatest Common Divider.
-     * @param a
-     * @param b
-     * @return
-     */
+
+    public static void main(String[] args) {
+        System.err.println("GCD for 24 and 54 = " + gcd2(54, 24));
+    }
+
+    /** Finds Greatest Common Divisor - Наибольший Общий Делитель.*/
     public static int gcd(int a, int b) {
+        while (a != 0 && b != 0) {
+            if (a >= b) {
+                a = a % b;
+            } else {
+                b = b % a;
+            }
+        }
+        return a + b; //One of them is null.
+    }
+    /**
+     * Recursive way.
+     */
+    public static int gcd2(int a, int b) {
         if (b == 0) {
             return a;
         } else {
