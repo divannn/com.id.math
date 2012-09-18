137553528a4e2a97a314d6b0a2d9569d53bbd47f
divannn <unique-id@mail.ru>
2012-09-18 11:58:07 +0400

    Added rotateLeft1().

---
+++ src/main/java/com/id/math/search/RotateArray.java
@@ -0,0 +1,82 @@
+package com.id.math.search;
+
+import com.id.math.util.ArrayUtils;
+
+/**
+ * Rotate array to the left cyclically.
+ * Having array: 1 2 3 4 5 6 7.
+ * Rotation of the above array by 2 will make array: 3 4 5 6 7 1 2.
+ */
+public class RotateArray {
+
+    public static void main(String[] args) {
+        int[] data = {-2, 1, 0, 4, 5};
+        ArrayUtils.printArray(data);
+        //rotateLeft1(data, 2);
+        rotateLeft2(data, 2);
+        ArrayUtils.printArray(data);
+    }
+
+    /**
+     * Time complexity: O(n*shift)
+     * Auxiliary Space: O(1)
+     */
+    public static void rotateLeft1(int[] array, int shift) {
+        if (shift <= 0 || array == null || array.length <= shift) {
+            throw new IllegalArgumentException("Invalid args");
+        }
+        for (int i = 0; i < shift; i++) {
+            leftRotateByOne(array);
+        }
+    }
+
+    /**
+     * Rotate by 1.
+     */
+    private static void leftRotateByOne(int[] array) {
+        int tmp = array[0];
+        for (int i = 0; i < array.length - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[array.length - 1] = tmp;
+    }
+
+    /**
+     * Better solution.
+     * Time complexity: O(n)
+     * Auxiliary Space: O(1)
+     */
+    public static void rotateLeft2(int[] array, int shift) {
+        if (shift <= 0 || array == null || array.length <= shift) {
+            throw new IllegalArgumentException("Invalid args");
+        }
+        int n = array.length;
+        int i, j, k, temp;
+        for (i = 0; i < gcd(shift, n); i++) {
+            /* move i-th values of blocks */
+            temp = array[i];
+            j = i;
+            while (true) {
+                k = j + shift;
+                if (k >= n) {
+                    k = k - n;
+                }
+                if (k == i) {
+                    break;
+                }
+                array[j] = array[k];
+                j = k;
+            }
+            array[j] = temp;
+        }
+    }
+
+    /*Function to get GCD of a and b*/
+    private static int gcd(int a, int b) {
+        if (b == 0) {
+            return a;
+        } else {
+            return gcd(b, a % b);
+        }
+    }
+}
