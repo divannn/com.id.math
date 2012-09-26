83db463dc19f3d8d11a2c4a39e60d8f094266080
divannn <unique-id@mail.ru>
2012-09-26 12:48:23 +0400

    Added 2d version,

--- src/main/java/com/id/math/numbers/GCD.java
+++ src/main/java/com/id/math/numbers/GCD.java
@@ -1,12 +1,17 @@
 package com.id.math.numbers;
 
+/**
+ * Finds Greatest Common Divisor - Наибольший Общий Делитель.
+ */
 public class GCD {
 
     public static void main(String[] args) {
-        System.err.println("GCD for 24 and 54 = " + gcd2(54, 24));
+        System.err.println("GCD for 24 and 54 = " + gcd2(24, 54));
     }
 
-    /** Finds Greatest Common Divisor - Наибольший Общий Делитель.*/
+    /**
+     * Via division.
+     */
     public static int gcd(int a, int b) {
         while (a != 0 && b != 0) {
             if (a >= b) {
@@ -15,12 +20,29 @@ public class GCD {
                 b = b % a;
             }
         }
-        return a + b; //One of them is null.
+        return a + b; //one of them is null.
     }
+
+    /**
+     * Via subtraction.
+     * Проще сформулировать алгоритм Евклида так: если даны натуральные числа  a и b, пока получается ненулевое число,
+     * по очереди вычитать из большего меньшее, то в результате получится НОД.
+     */
+    public static int gcd2(int a, int b) {
+        while (a != 0 && b != 0) {
+            if (a >= b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        return a + b; //one of them is null here.
+    }
+
     /**
      * Recursive way.
      */
-    public static int gcd2(int a, int b) {
+    public static int gcd3(int a, int b) {
         if (b == 0) {
             return a;
         } else {
