c48a2335db8d7293bc50c79222e23362c61193d2
divannn <unique-id@mail.ru>
2012-10-10 09:53:34 +0400

    Corrected factorial - pass parameter.

--- src/main/java/com/id/math/numbers/Factorial.java
+++ src/main/java/com/id/math/numbers/Factorial.java
@@ -4,36 +4,38 @@ import java.math.BigInteger;
 
 /**
  * @author idanilov
- *
  */
 public class Factorial {
 
-	private final static int NUM = 20;
+    public static void main(String[] args) {
+        factorial1(20);
+        factorial2(10);
+    }
 
-	public static void main(String[] args) {
-		factorial1();
-		factorial2();
-	}
+    //BigInteger solution.
+    private static void factorial1(int num) {
+        //BigInteger solution.
+        System.out.println("good numbers for n = " + num);
+        BigInteger n = BigInteger.ONE;
+        for (int i = 1; i <= num; i++) {
+            n = n.multiply(BigInteger.valueOf(i));
+            System.out.println(" " + i + "! = " + n);
+        }
+    }
 
-	//BigInteger solution.
-	private static void factorial1() {
-		//BigInteger solution.
-		System.out.println("good numbers for n = " + NUM);
-		BigInteger n = BigInteger.ONE;
-		for (int i = 1; i <= NUM; i++) {
-			n = n.multiply(BigInteger.valueOf(i));
-			System.out.println(i + "! = " + n);
-		}
-	}
+    //int solution (BAD IDEA BECAUSE ONLY WORKS TO 12).
+    private static void factorial2(int n) {
+        System.out.println("bad numbers for n = " + n);
+        int fact = 1;
+        for (int i = 1; i <= n; i++) {
+            fact = fact * i;
+            System.out.println(" " + i + "! = " + fact);
+        }
+    }
 
-	//int solution (BAD IDEA BECAUSE ONLY WORKS TO 12).
-	private static void factorial2() {
-		System.out.println("bad numbers for n = " + NUM);
-		int fact = 1;
-		for (int i = 1; i <= NUM; i++) {
-			fact = fact * i;
-			System.out.println(i + "! = " + fact);
-		}
-	}
+    //recursive way(also BAD).
+    private static int factorial3(int n) {
+        return (n == 0) ? 1 : n * factorial3(n - 1);
+    }
 
 }
