4cab53bffff20e72528a6f6fd6e78faa942a2cea
divannn <unique-id@mail.ru>
2012-10-10 09:56:02 +0400

    Added checks.

--- src/main/java/com/id/math/numbers/Factorial.java
+++ src/main/java/com/id/math/numbers/Factorial.java
@@ -8,34 +8,43 @@ import java.math.BigInteger;
 public class Factorial {
 
     public static void main(String[] args) {
-        factorial1(20);
+        factorial1(0);
         factorial2(10);
     }
 
     //BigInteger solution.
-    private static void factorial1(int num) {
+    private static void factorial1(int N) {
+        if (N < 0) {
+            throw new IllegalArgumentException("Value must be positive");
+        }
         //BigInteger solution.
-        System.out.println("good numbers for n = " + num);
-        BigInteger n = BigInteger.ONE;
-        for (int i = 1; i <= num; i++) {
-            n = n.multiply(BigInteger.valueOf(i));
-            System.out.println(" " + i + "! = " + n);
+        System.out.println("good numbers for n = " + N);
+        BigInteger next = BigInteger.ONE;
+        for (int i = 1; i <= N; i++) {
+            next = next.multiply(BigInteger.valueOf(i));
+            System.out.println(" " + i + "! = " + next);
         }
     }
 
     //int solution (BAD IDEA BECAUSE ONLY WORKS TO 12).
-    private static void factorial2(int n) {
-        System.out.println("bad numbers for n = " + n);
+    private static void factorial2(int N) {
+        if (N < 0) {
+            throw new IllegalArgumentException("Value must be positive");
+        }
+        System.out.println("bad numbers for n = " + N);
         int fact = 1;
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= N; i++) {
             fact = fact * i;
             System.out.println(" " + i + "! = " + fact);
         }
     }
 
     //recursive way(also BAD).
-    private static int factorial3(int n) {
-        return (n == 0) ? 1 : n * factorial3(n - 1);
+    private static int factorial3(int N) {
+        if (N < 0) {
+            throw new IllegalArgumentException("Value must be positive");
+        }
+        return (N == 0) ? 1 : N * factorial3(N - 1);
     }
 
 }
