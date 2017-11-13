e0a36156c799016d9483682f2630135e989a0db4
divannn <divannn@mail.ru>
2017-11-13 11:23:37 +0300

    MInor changes

--- src/main/java/com/id/math/numbers/GCD.java
+++ src/main/java/com/id/math/numbers/GCD.java
@@ -54,9 +54,8 @@ public class GCD {
     public static int gcd3(int a, int b) {
         if (b == 0) {
             return a;
-        } else {
-            return gcd3(b, a % b);
         }
+        return gcd3(b, a % b);
     }
 
 }
