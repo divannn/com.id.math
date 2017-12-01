b5db427e2c905b0781dcd1950a08ce4ecba5e23b
divannn <divannn@mail.ru>
2017-12-01 21:11:12 +0300

    Added new test

---
+++ src/main/java/com/id/math/random/Rand7ViaRand7.java
@@ -0,0 +1,43 @@
+package com.id.math.random;
+
+import org.junit.Test;
+
+import java.util.Random;
+
+import static org.junit.Assert.assertTrue;
+
+public class Rand7ViaRand7 {
+
+    /**
+     * Given rand5() implement rand7().
+     * rand5() generates a random number between O and 4 (inclusive),
+     */
+    public int rand7() {
+        while (true) {
+            int r1 = 2 * rand5();//evens in [0-8]
+            int r2 = rand5();//[0-4]
+            if (r2 != 4) {
+                int rand1 = r2 % 2;//0 or 1
+                int num = r1 + rand1;//will be in [0-9]
+                if (num < 7) {
+                    return num;
+                }
+            }
+        }
+    }
+
+    private int rand5() {
+        Random r = new Random();
+        return r.nextInt(5);
+    }
+
+    @Test
+    public void test() {
+        System.err.println("");
+        for (int i = 0; i < 1000; i++) {
+            int v = rand7();
+            assertTrue(v >= 0 && v < 7);
+        }
+    }
+
+}
