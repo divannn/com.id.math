f364c69c91182243841620638b7ef1cd37acd0b1
divannn <divannn@mail.ru>
2017-12-11 15:00:49 +0300

    Generate instead of print

---
+++ src/main/java/com/id/math/array/GenAllSubsets.java
@@ -0,0 +1,52 @@
+package com.id.math.array;
+
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+public class GenAllSubsets {
+
+    /**
+     * Generate all subsets of given set[]
+     */
+    public static List<List> genSubsets(char set[]) {
+        int n = set.length;
+        int numOfSubsets = 1 << n;
+        List<List> result = new ArrayList<>(numOfSubsets);
+
+        // Run a loop for printing all 2^n subsets
+        for (int i = 0; i < numOfSubsets; i++) {
+            List<Character> curSubset = new ArrayList<>(n);
+            // Gen current subset
+            for (int j = 0; j < n; j++) {
+                // map each bit of current number to array element
+                if ((i & (1 << j)) > 0) {
+                    curSubset.add(set[j]);
+                }
+            }
+            result.add(curSubset);
+        }
+
+        return result;
+    }
+
+    @Test
+    public void test() {
+        char set[] = {'a', 'b', 'c', 'd'};
+        List<List> r = genSubsets(set);
+        assertEquals(16, r.size());
+        assertTrue(r.get(0).size() == 0);
+    }
+
+    public static void main(String[] args) {
+        char set[] = {'a', 'b', 'c', 'd'};
+        List<List> r = genSubsets(set);
+        for (List list : r) {
+            System.err.println(list);
+        }
+    }
+}
