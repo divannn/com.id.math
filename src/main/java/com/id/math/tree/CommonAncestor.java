347eb6d65f8056ae8b831895bd401adcc7bda6a9
divannn <divannn@mail.ru>
2018-02-27 20:40:36 +0300

    added lca2

--- src/main/java/com/id/math/tree/CommonAncestor.java
+++ src/main/java/com/id/math/tree/CommonAncestor.java
@@ -9,15 +9,18 @@ public class CommonAncestor {
 
     /**
      * Given two nodes that already exist in a binary search tree, find the lowest common ancestor.
+     * Use ints just to ease comparing.
      *
      * @param root
      * @param one
      * @param two
-     * @return
+     * @return LCA for two nodes in binary search tree or null if there is no such
      */
-    //use ints just to ease comparing.
-    public static BinaryTreeNode<Integer> lca(BinaryTreeNode<Integer> root, BinaryTreeNode<Integer> one, BinaryTreeNode<Integer> two) {
-        // Check if one and two are in the root tree.
+    private static BinaryTreeNode<Integer> lcaInBinarySearchTree(BinaryTreeNode<Integer> root, BinaryTreeNode<Integer> one, BinaryTreeNode<Integer> two) {
+        if (root == null || one == null || two == null) {
+            return null;
+        }
+        // Check if one and two are in the same sub-tree.
         while (root != null) {
             if (root.data < one.data && root.data < two.data) {
                 root = root.right;
@@ -32,13 +35,40 @@ public class CommonAncestor {
 
 
     /**
-     * Find lower common acnestor of two arbitrary nodes preseneted in binary tree.
+     * Find lower common ancestor of two arbitrary nodes presented in binary tree.
      *
-     * @param one
-     * @param two
+     * @param root of the
+     * @param one  1st node
+     * @param two  2nd node
      * @return node that us LCA for both nodes
      */
-    public static <T> NodeWithParent<T> lca2(NodeWithParent<T> one, NodeWithParent<T> two) {
+    private static BinaryTreeNode<Integer> lca2(BinaryTreeNode<Integer> root, BinaryTreeNode<Integer> one, BinaryTreeNode<Integer> two) {
+        if (root == null) {
+            return null;
+        }
+        if (one == root || two == root) {
+            return root;
+        }
+        BinaryTreeNode left = lca2(root.left, one, two);
+        BinaryTreeNode right = lca2(root.right, one, two);
+        if (left != null && right != null) {
+            return root;  // if both nodes and on different sides - we found LCA.
+        }
+        return left != null ? left : right;
+    }
+
+    /**
+     * Find lower common ancestor of two arbitrary nodes presented in binary tree.
+     * Each node knows it parent only.
+     *
+     * @param one 1st node
+     * @param two 2nd node
+     * @return node that us LCA for both nodes
+     */
+    private static <T> NodeWithParent<T> lca3(NodeWithParent<T> one, NodeWithParent<T> two) {
+        if (one == null || two == null) {
+            return null;
+        }
         NodeWithParent<T> curr = one;
         int dist1 = 0;
         while (curr != null) {
@@ -74,16 +104,15 @@ public class CommonAncestor {
         return lo;
     }
 
-
     @Test
-    public void testLCA2() {
-        NodeWithParent<Integer> root = new NodeWithParent<>(null, 1);
-        NodeWithParent<Integer> n2 = new NodeWithParent<>(root, 2);
-        NodeWithParent<Integer> n3 = new NodeWithParent<>(root, 3);
-        NodeWithParent<Integer> n4 = new NodeWithParent<>(n2, 4);
-        NodeWithParent<Integer> n5 = new NodeWithParent<>(n2, 5);
-        NodeWithParent<Integer> n6 = new NodeWithParent<>(n3, 6);
-        NodeWithParent<Integer> n7 = new NodeWithParent<>(n6, 7);
+    public void testLCA1() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(4);
+        BinaryTreeNode<Integer> n2 = new BinaryTreeNode<>(2);
+        BinaryTreeNode<Integer> n3 = new BinaryTreeNode<>(5);
+        BinaryTreeNode<Integer> n4 = new BinaryTreeNode<>(1);
+        BinaryTreeNode<Integer> n5 = new BinaryTreeNode<>(3);
+        BinaryTreeNode<Integer> n6 = new BinaryTreeNode<>(6);
+        BinaryTreeNode<Integer> n7 = new BinaryTreeNode<>(7);
 
         root.left = n2;
         root.right = n3;
@@ -92,11 +121,54 @@ public class CommonAncestor {
         n2.right = n5;
 
         n3.right = n6;
-        n6.left = n7;
+        n6.right = n7;
 
-        assertEquals(n2, lca2(n4, n5));
-        assertEquals(n6, lca2(n6, n7));
-        assertEquals(root, lca2(n5, n7));
+        assertEquals(n2, lcaInBinarySearchTree(root, n4, n5));
+        assertEquals(n6, lcaInBinarySearchTree(root, n6, n7));
+        assertEquals(root, lcaInBinarySearchTree(root, n5, n7));
+        assertEquals(n5, lcaInBinarySearchTree(root, n5, n5));
+    }
+
+
+    @Test
+    public void testLCA2() {
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<>(4);
+        BinaryTreeNode<Integer> n2 = new BinaryTreeNode<>(2);
+        BinaryTreeNode<Integer> n3 = new BinaryTreeNode<>(5);
+        BinaryTreeNode<Integer> n4 = new BinaryTreeNode<>(1);
+        BinaryTreeNode<Integer> n5 = new BinaryTreeNode<>(3);
+        BinaryTreeNode<Integer> n6 = new BinaryTreeNode<>(6);
+        BinaryTreeNode<Integer> n7 = new BinaryTreeNode<>(7);
+
+        root.left = n2;
+        root.right = n3;
+
+        n2.left = n4;
+        n2.right = n5;
+
+        n3.right = n6;
+        n6.right = n7;
+
+        assertEquals(n2, lca2(root, n4, n5));
+        assertEquals(n6, lca2(root, n6, n7));
+        assertEquals(root, lca2(root, n5, n7));
+        assertEquals(n5, lca2(root, n5, n5));
+    }
+
+    @Test
+    public void testLCA3() {
+        NodeWithParent<Integer> root = new NodeWithParent<>(null, 1);
+        NodeWithParent<Integer> n2 = new NodeWithParent<>(root, 2);
+        NodeWithParent<Integer> n3 = new NodeWithParent<>(root, 3);
+        NodeWithParent<Integer> n4 = new NodeWithParent<>(n2, 4);
+        NodeWithParent<Integer> n5 = new NodeWithParent<>(n2, 5);
+        NodeWithParent<Integer> n6 = new NodeWithParent<>(n3, 6);
+        NodeWithParent<Integer> n7 = new NodeWithParent<>(n6, 7);
+
+        assertEquals(n2, lca3(n4, n5));
+        assertEquals(n6, lca3(n6, n7));
+        assertEquals(root, lca3(n5, n7));
+        assertEquals(n5, lca3(n5, n5));
     }
 
 }
