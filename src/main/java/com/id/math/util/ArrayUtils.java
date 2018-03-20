8750746d26ead696aa0c47fcec8dd0712db74d0f
divannn <divannn@mail.ru>
2018-03-20 14:13:13 +0300

    Simplify

--- src/main/java/com/id/math/util/ArrayUtils.java
+++ src/main/java/com/id/math/util/ArrayUtils.java
@@ -2,71 +2,68 @@ package com.id.math.util;
 
 /**
  * @author idanilov
- *
  */
 public final class ArrayUtils {
 
-	public static int[] copy(int[] src) {
-		int[] result = new int[src.length];
-		System.arraycopy(src, 0, result, 0, src.length);
-		return result;
-	}
+    public static int[] copy(int[] src) {
+        int[] result = new int[src.length];
+        System.arraycopy(src, 0, result, 0, src.length);
+        return result;
+    }
 
-	public static void swap(int[] data, int i, int j) {
-		if (data == null || i < 0 || j < 0 || data.length <= i || data.length <= j) {
-			throw new IllegalArgumentException();
-		}
-		int tmp = data[i];
-		data[i] = data[j];
-		data[j] = tmp;
-	}
+    public static void swap(int[] data, int i, int j) {
+        if (data == null || i < 0 || j < 0 || data.length <= i || data.length <= j) {
+            throw new IllegalArgumentException();
+        }
+        int tmp = data[i];
+        data[i] = data[j];
+        data[j] = tmp;
+    }
 
-	public static void printArray(int[] data) {
-		for (int n : data) {
-			System.err.print(n + " ");
-		}
-		System.err.println();
-	}
+    public static void printArray(int[] data) {
+        for (int n : data) {
+            System.err.print(n + " ");
+        }
+        System.err.println();
+    }
 
-	/**
-	 * @param data
-	 * @param l left index inclusive
-	 * @param r right index exclusive
-	 * @return miminal element in array
-	 */
-	public static int min(int[] data, int l, int r) {
-		checkParams(data, l, r);
-		int result = data[l];
-		for (int i = l; i < r; i++) {
-			if (data[i] < result) {
-				result = data[i];
-			}
-		}
-		return result;
-	}
+    /**
+     * @param data
+     * @param l    left index inclusive
+     * @param r    right index exclusive
+     * @return miminal element in array
+     */
+    public static int min(int[] data, int l, int r) {
+        checkParams(data, l, r);
+        int result = data[l];
+        for (int i = l; i < r; i++) {
+            if (data[i] < result) {
+                result = data[i];
+            }
+        }
+        return result;
+    }
 
-	/**
-	 * @param data
-	 * @param left left index inclusive
-	 * @param right right index exclusive
-	 * @return index of minimal element in array
-	 */
-	public static int minIndex(int[] data, int left, int right) {
-		checkParams(data, left, right);
-		int result = left;
-		int min = data[left];
-		for (int i = left; i < right; i++) {
-			if (data[i] < min) {
-				result = i;
-				min = data[i];
-			}
-		}
-		return result;
-	}
+    /**
+     * @param data
+     * @param left  left index inclusive
+     * @param right right index exclusive
+     * @return index of minimal element in array
+     */
+    public static int minIndex(int[] data, int left, int right) {
+        checkParams(data, left, right);
+        int result = left;
+        for (int i = left; i < right; i++) {
+            if (data[i] < data[result]) {
+                result = i;
+            }
+        }
+        return result;
+    }
 
-	private static void checkParams(int[] data, int l, int r) {
-		if (data == null || l < 0 || r < 0 || l > r || data.length < l || data.length < r) {
-			throw new IllegalArgumentException();
-		}
-	}
+    private static void checkParams(int[] data, int l, int r) {
+        if (data == null || l < 0 || r < 0 || l > r || data.length < l || data.length < r) {
+            throw new IllegalArgumentException();
+        }
+    }
 }
