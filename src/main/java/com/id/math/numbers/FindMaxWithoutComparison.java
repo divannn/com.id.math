f135c1f034650a48a1357050ff633eda66a26cc5
divannn <divannn@mail.ru>
2018-04-23 11:10:00 +0300

    Added new problem

---
+++ src/main/java/com/id/math/numbers/FindMaxWithoutComparison.java
@@ -0,0 +1,34 @@
+package com.id.math.numbers;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+
+public class FindMaxWithoutComparison {
+
+    /**
+     * Find max amnog of two without using cmparison operations.
+     */
+    public static int max(int a, int b) {
+        int diff = a - b;//overflow may occur here - disregard.
+        int sign = diff >>> 31;
+        System.err.println(">>> " + sign);
+        return a - sign * diff;
+    }
+
+    @Test
+    public void test() {
+        assertEquals(5, max(2, 5));
+        assertEquals(4, max(0, 4));
+        assertEquals(3, max(3, 1));
+        assertEquals(2, max(2, 2));
+        assertEquals(0, max(0, 0));
+
+        assertEquals(-2, max(-2, -2));
+        assertEquals(3, max(3, -1));
+        assertEquals(1, max(-3, 1));
+        assertEquals(-8, max(-8, -10));
+    }
+
+}
