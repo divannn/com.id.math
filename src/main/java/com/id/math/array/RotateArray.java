f4319f83a0e0e07b5ec88a3a7a4129d64f272781
divannn <divannn@mail.ru>
2018-04-26 15:33:51 +0300

    Added smart way - via 3 reverses.

--- src/main/java/com/id/math/array/RotateArray.java
+++ src/main/java/com/id/math/array/RotateArray.java
@@ -1,7 +1,8 @@
 package com.id.math.array;
 
-import com.id.math.util.ArrayUtils;
-import com.id.math.numbers.GCD;
+import org.junit.Test;
+
+import static org.junit.Assert.assertArrayEquals;
 
 /**
  * Rotate array to the left cyclically.
@@ -10,17 +11,9 @@ import com.id.math.numbers.GCD;
  */
 public class RotateArray {
 
-    public static void main(String[] args) {
-        int[] data = {-2, 1, 0, 4, 5};
-        ArrayUtils.printArray(data);
-        //rotateLeft1(data, 2);
-        rotateLeft2(data, 2);
-        ArrayUtils.printArray(data);
-    }
-
     /**
-     * Time complexity: O(n*shift)
-     * Auxiliary Space: O(1)
+     * @complexity O(n * shift)
+     * Space: O(1)
      */
     public static void rotateLeft1(int[] array, int shift) {
         if (shift <= 0 || array == null || array.length <= shift) {
@@ -32,7 +25,21 @@ public class RotateArray {
     }
 
     /**
-     * Rotate by 1.
+     * @complexity O(n)
+     * Space: O(1)
+     * Not mine - really smart!
+     */
+    public static void rotateLeft2(int[] array, int shift) {
+        if (shift <= 0 || array == null || array.length <= shift) {
+            throw new IllegalArgumentException("Invalid args");
+        }
+        reverse(array, 0, shift - 1);
+        reverse(array, shift, array.length - 1);
+        reverse(array, 0, array.length - 1);
+    }
+
+    /**
+     * Rotate array to the left by 1.
      */
     private static void leftRotateByOne(int[] array) {
         int tmp = array[0];
@@ -42,34 +49,57 @@ public class RotateArray {
         array[array.length - 1] = tmp;
     }
 
+
     /**
-     * Better solution.
-     * Time complexity: O(n)
-     * Auxiliary Space: O(1)
+     * Reverses array in place
+     *
+     * @param array
+     * @param start index inclusive
+     * @param end   index inclusive
      */
-    public static void rotateLeft2(int[] array, int shift) {
-        if (shift <= 0 || array == null || array.length <= shift) {
+    public static void reverse(int[] array, int start, int end) {
+        if (array == null || start > array.length || end > array.length || start > end) {
             throw new IllegalArgumentException("Invalid args");
         }
-        int n = array.length;
-        int i, j, k, temp;
-        for (i = 0; i < GCD.gcd1(shift, n); i++) {
-            /* move i-th values of blocks */
-            temp = array[i];
-            j = i;
-            while (true) {
-                k = j + shift;
-                if (k >= n) {
-                    k = k - n;
-                }
-                if (k == i) {
-                    break;
-                }
-                array[j] = array[k];
-                j = k;
-            }
-            array[j] = temp;
+        int len = end - start + 1;
+        for (int i = start; i < start + len / 2; i++) {
+            int tmp = array[i];
+            array[i] = array[start + end - i];
+            array[start + end - i] = tmp;
         }
+    }
+
+    @Test
+    public void testReverse() {
+        int[] arr = {-2, 1, 0, 4, 5};
+        reverse(arr, 0, arr.length - 1);
+        assertArrayEquals(new int[]{5, 4, 0, 1, -2}, arr);
+
+        arr = new int[]{1};
+        reverse(arr, 0, arr.length - 1);
+        assertArrayEquals(new int[]{1}, arr);
+
+        arr = new int[]{-2, 1, 0, 4, 5, 8};
+        reverse(arr, 3, arr.length - 1);
+        assertArrayEquals(new int[]{-2, 1, 0, 8, 5, 4}, arr);
+
+        arr = new int[]{-2, 1, 0, 4, 5, 8};
+        reverse(arr, 1, 4);
+        assertArrayEquals(new int[]{-2, 5, 4, 0, 1, 8}, arr);
+    }
+
+    @Test
+    public void test1() {
+        int[] arr = {-2, 1, 0, 4, 5};
+        rotateLeft1(arr, 2);
+        assertArrayEquals(new int[]{0, 4, 5, -2, 1}, arr);
+    }
+
+    @Test
+    public void test2() {
+        int[] arr = {-2, 1, 0, 4, 5};
+        rotateLeft2(arr, 2);
+        assertArrayEquals(new int[]{0, 4, 5, -2, 1}, arr);
     }
 
 }
