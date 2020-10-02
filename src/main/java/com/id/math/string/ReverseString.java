3ebdffc20808d33dfd51f80ed92bdf59f9adf45d
divannn <divannn@mail.ru>
2020-10-02 18:21:47 +0300

    Added 2nd way

--- src/main/java/com/id/math/string/ReverseString.java
+++ src/main/java/com/id/math/string/ReverseString.java
@@ -8,17 +8,34 @@ public class ReverseString {
 
     private static String reverseString(String s) {
         if (s == null) {
-            return "";
+            throw new IllegalArgumentException("Invalid input");
         }
-        char[] str = s.toCharArray();
-        int len = str.length;
+        char[] chars = s.toCharArray();
+        int len = chars.length;
         for (int i = 0; i < len / 2; i++) {
-            char tmp = str[i];
+            char tmp = chars[i];
             int lastInd = len - i - 1;
-            str[i] = str[lastInd];
-            str[lastInd] = tmp;
+            chars[i] = chars[lastInd];
+            chars[lastInd] = tmp;
         }
-        return new String(str);
+        return new String(chars);
+    }
+
+    private static String reverseString2(String s) {
+        if (s == null) {
+            throw new IllegalArgumentException("Invalid input");
+        }
+        char[] chars = s.toCharArray();
+        int l = 0;
+        int r = chars.length - 1;
+        while (l < r) {
+            char tmp = chars[l];
+            chars[l] = chars[r];
+            chars[r] = tmp;
+            l++;
+            r--;
+        }
+        return new String(chars);
     }
 
     @Test
@@ -26,6 +43,13 @@ public class ReverseString {
         assertEquals("1", reverseString("1"));
         assertEquals("ba", reverseString("ab"));
         assertEquals("54321dcba", reverseString("abcd12345"));
+    }
+
+    @Test
+    public void test2() {
+        assertEquals("1", reverseString2("1"));
+        assertEquals("ba", reverseString2("ab"));
+        assertEquals("54321dcba", reverseString2("abcd12345"));
     }
 
 }
